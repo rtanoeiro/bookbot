a564ebfa883f8a58d9bc2308924b9ba84a73a3df
Ramon Tanoeiro <ramon.tanoeiro@indebted.co>
2024-11-11 16:20:23 +1100

    Added functions to count number of words and of each character

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+def count_characters(text: str) -> dict[str, int]:
+    character_dictionary = {}
+
+    for character in text:
+        if character in character_dictionary:
+            character_dictionary[character] += 1
+        else:
+            character_dictionary[character] = 1
+
+    return character_dictionary
+
+
 def main():
     with open("books/frankenstein.txt") as f:
-        text = f.read()
+        text = (f.read()).lower()
+        num_words = len(text.split())
+        char_count = count_characters(text)
 
-    num_words = len(text.split())
     print(f"The number of words in the book is: {num_words}")
+    print(f"The character dictionary for the text is: {char_count}")
 
 
 main()
