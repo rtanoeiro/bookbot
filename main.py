c4504a35f78a51e34e776826d69e533f7eef37ad
Ramon Tanoeiro <ramon.tanoeiro@indebted.co>
2024-11-11 16:36:30 +1100

    add sort of dict and remove spaces and line breaks in count

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ def count_characters(text: str) -> dict[str, int]:
     character_dictionary = {}
 
     for character in text:
+        if character in (" ", "\n"):
+            continue
         if character in character_dictionary:
             character_dictionary[character] += 1
         else:
             character_dictionary[character] = 1
 
-    return character_dictionary
+    sorted_char = sorted(character_dictionary.items(), key=lambda x: x[1], reverse=True)
+    sorted_char_dict = dict(sorted_char)
+
+    return sorted_char_dict
 
 
 def create_book_report(char_dict: dict[str, int]) -> None:
