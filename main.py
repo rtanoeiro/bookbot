950996759b23e6e0673b36d26c2607130aa68ab1
rtanoeiro <ramontanoeiro@gmail.com>
2025-03-23 22:56:26 +1100

    updated main code report

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 from stats import count_characters, count_words
+import sys
 
 
-def create_book_report(book_path: str) -> None:
+def get_word_count(book_path: str) -> int:
     with open(book_path) as f:
         text = (f.read()).lower()
 
-    num_words = count_words(text)
-    print(f"{num_words} words found in the document")
+    return count_words(text)
 
 
-def main():
-    print("--- Begin report ---")
-    create_book_report("books/frankenstein.txt")
-    print("--- End report ---")
+def get_character_count(book_path: str) -> dict[str, int]:
+    with open(book_path) as f:
+        text = (f.read()).lower()
+
+    return count_characters(text)
 
 
-main()
+def main(file_path):
+    num_words = get_word_count(file_path)
+    character_dict = get_character_count(file_path)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for character, count in character_dict.items():
+        if character.isalpha():
+            print(f"{character}: {count}")
+    print("============= END ===============")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    main(file_path)
